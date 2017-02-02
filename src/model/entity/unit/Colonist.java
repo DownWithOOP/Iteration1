199317e13dc2de08272de19e424b78c656d4237e
Leslie Richardson <bookworm42795@gmail.com>
2017-02-02 11:36:12 -0500

    Compiling should really work now

--- src/model/entity/unit/Colonist.java
+++ src/model/entity/unit/Colonist.java
@@ -8,14 +8,9 @@ public class Colonist extends FighterUnit {
 
     }
 
-<<<<<<< HEAD
-//    public Base buildStructure(int rowPosition, int columnPosition) {
-//        return new Base(rowPosition, columnPosition);
+
+//    public Base buildStructure(int rowPosition, int columnPosition, int visionRadius) {
+//        return new Base(rowPosition, columnPosition,visionRadius);
 //    }
-=======
-    public Base buildStructure(int rowPosition, int columnPosition, int visionRadius) {
-        return new Base(rowPosition, columnPosition,visionRadius);
-    }
->>>>>>> 55ab92aee7532f1a8f76298483f80e5b7d1f45cf
 
 }

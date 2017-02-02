960d77fe2fa5afd9e522ec02049f5a641077a909
cduica <crisd0000@gmail.com>
2017-02-02 18:18:16 -0500

    added player skeleton class

--- src/model/player/Player.java
+++ src/model/player/Player.java
@@ -1,8 +1,53 @@
 package model.player;
 
+import model.actions.Action;
+import model.entity.Entity;
+import model.entity.army.Army;
+import model.entity.structure.Structure;
+import model.entity.unit.Unit;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
- * Created by jordi on 2/2/2017.
+ * Created by CRISTIAN aka Batman
  */
 public class Player {
 
+    private ArrayList<Entity> allEntities;
+    private ArrayList<Unit> units;
+    private ArrayList<Structure> structures;
+    private ArrayList<Army> armies;
+
+    private Entity selectedEntity;
+
+    private HashMap<Integer, Action> actionMap;
+
+    public Player(){
+
+    }
+
+    public boolean addStructure(Structure structure){
+        return false;
+    }
+
+    public boolean addUnit(Unit unit){
+        return false;
+    }
+
+    public boolean addArmy(Army army){
+        return false;
+    }
+
+    public boolean removeStructure(){
+        return false;
+    }
+
+    public boolean removeUnit(){
+        return false;
+    }
+
+    public boolean removeArmy(){
+        return false;
+    }
 }

46d19620db94517766103e98539940813ca25e61
Leslie Richardson <bookworm42795@gmail.com>
2017-02-02 14:47:56 -0500

    Finished stat-related classes and added constructors for all entity-based classes

--- src/model/entity/unit/Colonist.java
+++ src/model/entity/unit/Colonist.java
@@ -5,11 +5,11 @@ import model.entity.structure.Base;
 public class Colonist extends FighterUnit {
 
     public Colonist() {
-
+        super(2, 1, 0, 3, 50, 1, 5);
     }
 
 
-//    public Base buildStructure(int rowPosition, int columnPosition, int visionRadius) {
+//    public Base buildStructure(int rowPosition, int columnPosition) {
 //        return new Base(rowPosition, columnPosition,visionRadius);
 //    }
 

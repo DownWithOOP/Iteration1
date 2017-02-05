3b5b22f93a50afc0146baf9fbeba2df2c94bced5
larryelantz <umbranox@ufl.edu>
2017-02-05 15:12:25 -0500

    Update OneShotItem.java

--- src/model/map/tile/item/OneShotItem.java
+++ src/model/map/tile/item/OneShotItem.java
@@ -4,7 +4,9 @@ package model.map.tile.item;
  * Created by cduica on 2/4/17.
  */
 public class OneShotItem extends Item {
-  public void affectUnit(Unit unit){
+	public ItemEffect itemEffect;
+	
+  	public void affectUnit(Unit unit){
 		
 	}
 }

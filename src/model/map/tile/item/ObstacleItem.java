5c0abfa9f4fe012756ab67fbb6caa9dae80a554e
larryelantz <umbranox@ufl.edu>
2017-02-05 21:50:11 -0500

    Update ObstacleItem.java

--- src/model/map/tile/item/ObstacleItem.java
+++ src/model/map/tile/item/ObstacleItem.java
@@ -2,16 +2,17 @@ package model.map.tile.item;
 
 
 import model.common.Location;
+import model.map.tile.Tile;
 
 /**
  * Created by cduica on 2/4/17.
  */
 public class ObstacleItem extends Item {
     public ObstacleItem(Location location) {
-        super(location);
+        super(location, 0);
         ObstacleItemEffect();
     }
-    public ObstacleItemEffect(){
-        this.isPassable = false;
+    public void ObstacleItemEffect(){
+        Tile.isPassable = false;
     }
 }

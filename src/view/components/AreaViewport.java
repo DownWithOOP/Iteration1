91eedefe039a311eada740fc4385aa25a79e83f4
Jonathen Settle <jonathenksettle@ufl.edu>
2017-02-05 19:16:08 -0500

    Created TilePanel to handle drawing tiles on AreaViewport. Started laying groundwork to put TilePanels on a grid. Changed how images are loaded for Tiles.

--- src/view/components/AreaViewport.java
+++ src/view/components/AreaViewport.java
@@ -1,45 +1,33 @@
 package view.components;
 
 import model.common.Location;
-import model.common.RenderObject;
 import model.map.Map;
 import model.map.tile.Tile;
-import org.w3c.dom.css.Rect;
 
 import javax.swing.JPanel;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AreaViewport extends JPanel {
 
-    private Image[][] tiles;
+    private TilePanel[][] tiles;
     private Rectangle bounds;
 
     private Location mapCenter;
 
-    private int tileHieght;
+    private int tileHeight;
     private int tileWidth;
     private int numTileRows;
     private int numTileCols;
 
 
-    private Image waterImage;
-    private Image craterImage;
-    private Image grassImage;
-    private Image dirtImage;
+    private static final String waterImagePath = "res/images/water.png";
+    private static final String craterImagePath = "res/images/crater.png";
+    private static final String grassImagePath = "res/images/grass.png";
+    private static final String dirtImagePath = "res/images/dirt.png";
 
 
-    public AreaViewport(Map initalMap, Location initialLocation){
-        //Get images
-        Toolkit tool = Toolkit.getDefaultToolkit();
-        waterImage = tool.getImage("res/images/water.png");
-        craterImage = tool.getImage("res/images/crater.png");
-        grassImage = tool.getImage("res/images/grass.png");
-        dirtImage = tool.getImage("res/images/dirt.png");
-
-
-
+    public AreaViewport(LayoutManager layout, Map initialMap, Location initialLocation){
+        super(layout);
         setBackground(Color.black);
         setVisible(true);
 
@@ -47,52 +35,51 @@ public class AreaViewport extends JPanel {
 
         bounds = new Rectangle(getSize());
 
-        //TODO determine num tiles from ht/wid from images.
+        //TODO find way to either get bounds properly or work around the fact that we don't know bounds
         numTileRows = 3; //(int) bounds.getHeight() / get;
         numTileCols = 3; //(int) bounds.getWidth();
 
-        tileHieght = (int) bounds.getHeight()/numTileRows;
+        tileHeight = (int) bounds.getHeight()/numTileRows;
         tileWidth = (int) bounds.getWidth()/numTileCols;
 
-        tiles = new Image[initalMap.getHeight()][initalMap.getWidth()];
+        tiles = new TilePanel[numTileRows][numTileCols];
 
         mapCenter = initialLocation;
 
-        updateTiles(initalMap);
+        updateTiles(initialMap);
     }
 
     private void updateTiles(Map map) {
         System.out.println("SIZE IS HUUUUUUUUUUUUUUUGE" + getSize());
         for (int row = 0; row < numTileRows; ++row){
             for (int col = 0; col < numTileCols; ++col){
+                if(tiles[row][col] != null) {
+                    remove(tiles[row][col]);
+                }
                 //TODO handle when actualYCoord and actualXCoord go off of the map
                 int actualYCoord = mapCenter.getyCoord() - ( numTileRows/2 - row);
                 int actualXCoord = mapCenter.getxCoord() - ( numTileCols/2 - col);
                 Tile currentTile = map.getTile(actualYCoord, actualXCoord);
                 switch (currentTile.getTerrain().getTerrainType()) {
                     case DIRT:
-                        tiles[row][col] = dirtImage;
-                        System.out.println("dirt");
+                        tiles[row][col] = new TilePanel(dirtImagePath);
                         break;
                     case CRATER:
-                        tiles[row][col] = craterImage;
-                        System.out.println("crater");
+                        tiles[row][col] = new TilePanel(craterImagePath);
                         break;
                     case GRASS:
-                        tiles[row][col] = grassImage;
-                        System.out.println("grass");
+                        tiles[row][col] = new TilePanel(grassImagePath);
                         break;
                     case WATER:
-                        tiles[row][col] = waterImage;
-                        System.out.println("water");
+                        tiles[row][col] = new TilePanel(waterImagePath);
                         break;
                     default:
-                        tiles[row][col] = dirtImage;
-                        System.out.println("nada");
+                        tiles[row][col] = new TilePanel(dirtImagePath);
                         break;
                 }
                 //TODO look up image based on tile type
                 //TODO get entityID from tile and parse
+                add(tiles[row][col]);
             }
         }
     }
@@ -101,7 +88,7 @@ public class AreaViewport extends JPanel {
         System.out.println("SIZE IS HUUUUUUUUUUUUUUUGE" + getSize());
         System.out.println("AREAVIEWPORT X LOCATION IS HUUUUUUUUUUUUUUUGE " + getX());
         System.out.println("AREAVIEWPORT Y LOCATION IS HUUUUUUUUUUUUUUUGE " + getY());
-        tileHieght = (int) bounds.getHeight()/numTileRows;
+        tileHeight = (int) bounds.getHeight()/numTileRows;
         tileWidth = (int) bounds.getWidth()/numTileCols;
         mapCenter = updatedMapCenter;
         updateTiles(updatedMap);
@@ -110,14 +97,6 @@ public class AreaViewport extends JPanel {
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);//do I need this??? IDK
-        Graphics2D g2 = (Graphics2D) g;
-        for (int row = 0; row < numTileRows; ++row){
-            for (int col = 0; col < numTileCols; ++col){
-                //TODO look up image based on tile type
-                //TODO get entityID from tile and parse
-                g.drawImage(tiles[row][col], (col+1)*getX(), (row+1)*getY(), getWidth(), getHeight(), this);
-            }
-        }
         System.out.println("PAINTING AREA VIEWPORT");
     }
 }
---
+++ src/view/components/TilePanel.java
@@ -0,0 +1,51 @@
+package view.components;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.Buffer;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class TilePanel extends JPanel {
+
+    private BufferedImage tileImage;
+    private Rectangle bounds;
+
+    private static final int TILE_WIDTH = 250;
+    private static final int TILE_HEIGHT = 250;
+
+    public TilePanel(String filename) {
+
+        setPreferredSize(new Dimension(TILE_WIDTH,TILE_HEIGHT));
+
+        //Get image
+        try {
+            String currentDir = "file:///" + System.getProperty("user.dir").toString().replace("\\", "/");
+            BufferedImage rawImage  = ImageIO.read(new URL(currentDir + "/" + filename));
+
+            //Image scaledImage = rawImage.getScaledInstance(TILE_WIDTH, TILE_HEIGHT, Image.SCALE_SMOOTH);
+            //System.out.println(scaledImage.getWidth(null));
+
+            //tileImage = new BufferedImage(scaledImage.getWidth(null), scaledImage.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+            tileImage = new BufferedImage(TILE_WIDTH, TILE_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D tileImageGraphics = tileImage.createGraphics();
+            tileImageGraphics.drawImage(rawImage, 0, 0, TILE_WIDTH, TILE_HEIGHT, null);
+            tileImageGraphics.dispose();
+
+            System.out.println(tileImage.getWidth());
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        g.drawImage(tileImage, 0, 0, null);
+    }
+}
+
